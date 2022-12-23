df76e08b1fcb438ad91cb869edffe3b346a1f115
VETambiev <VETambiev@sbcloud.ru>
2022-12-23 16:35:40 +0300

    requirements.txt, new tests, refactored conftests.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import logging
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 LOG = logging.getLogger(__name__)
 LOG_FILTER = {
@@ -26,15 +28,28 @@ for name, level in LOG_FILTER.items():
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='ru')
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    language = request.config.getoption("language")
-    browser = webdriver.Chrome()
-    link = f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/'
     LOG.info('start browser')
-    browser.get(link)
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption('language')
+    if browser_name == "chrome":
+        options = ChromeOptions()
+        options.add_experimental_option('prefs', {
+            'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+        options.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
     yield browser
+
     LOG.info('quit browser')
     browser.quit()
